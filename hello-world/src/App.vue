2bdc9b05b0916285c6d160577913b89b4bab99c2
imenelhakim <imen.elhakim@esprit.tn>
2023-02-01 15:35:54 +0100

    binding classes

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -7,6 +7,21 @@
   <!-- binding to attributes -->
   <h2 v-bind:id="headingId">Hey</h2>
   <button v-bind:disabled="isDisabled">Bind</button>
+  <!-- binding classes -->
+  <h2 class="underline">Underlined text</h2>
+  <h2 class="underline" v-bind:class="status">Status</h2>
+  <h2 v-bind:class="isPromoted && 'promoted'">Promoted movie</h2>
+  <!-- binding classes with ternary operator -->
+  <h2 v-bind:class="isSoldOut ? 'sold-out' : 'new'">Soldout movie?</h2>
+  <h2 v-bind:class="'new promoted'">Newly promoted movie</h2>
+  <!-- binding classes : array -->
+  <h2 v-bind:class="[isPromoted && 'promoted', isSoldOut ? 'sold-out' : 'new']">Array conditional rendering</h2>
+  <!-- binding classes : object -->
+  <h2 v-bind:class="{
+    promoted: isPromoted,
+    new: !isSoldOut,
+    'sold-out': isSoldOut
+  }">Array conditional rendering</h2>
 </template>
 
 <script>
@@ -23,7 +38,10 @@ export default {
       name:"Imen",
       hack: `<a href="#" onClick="alert('hacked 3:)')">Click to win a prize!<a/>`,
       headingId: 'heading',
-      isDisabled: false
+      isDisabled: false,
+      status:'danger',
+      isPromoted: true,
+      isSoldOut: true
     }
   }
 }
@@ -38,4 +56,24 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.underline{
+  text-decoration: underline;
+}
+
+.danger{
+  color: red;
+}
+
+.promoted{
+  font-style: italic;
+}
+
+.new{
+  color: green;
+}
+.sold-out{
+  color: rgb(165, 22, 22);
+}
+
 </style>
